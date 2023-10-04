c49e46015206ab7e7d1959f56fe0239ccd94cf49
SebastianAdamoszek <sebaadamoszek@vp.pl>
2023-10-05 00:39:07 +0200

    add gallery

--- src/imageApi.js
+++ src/imageApi.js
@@ -14,49 +14,51 @@ const lightbox = new SimpleLightbox('.gallery a', {
 });
 
 function createImageCard(image) {
-  const {
-    webformatURL,
-    largeImageURL,
-    tags,
-    likes,
-    views,
-    comments,
-    downloads,
-  } = image;
+    const {
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    } = image;
+  
+    const photoCard = document.createElement('div');
+    photoCard.classList.add('photo-card');
+  
+    const aThumbnail = document.createElement('a');
+    aThumbnail.href = largeImageURL; // Link do dużego zdjęcia
+    aThumbnail.setAttribute('data-lightbox', 'gallery');
+  
+    const img = document.createElement('img');
+    img.src = webformatURL; // Małe zdjęcie
+    img.alt = tags;
+    img.loading = 'lazy';
+  
+    const info = document.createElement('div');
+    info.classList.add('info');
+  
+    function createInfoItem(label, value) {
+      const element = document.createElement('p');
+      element.classList.add('info-item');
+      element.textContent = `${label}: ${value}`;
+      return element;
+    }
+  
+    info.appendChild(createInfoItem('Likes', likes));
+    info.appendChild(createInfoItem('Views', views));
+    info.appendChild(createInfoItem('Comments', comments));
+    info.appendChild(createInfoItem('Downloads', downloads));
+  
+    aThumbnail.appendChild(img);
+    photoCard.appendChild(aThumbnail);
 
-  const photoCard = document.createElement('div');
-  photoCard.classList.add('photo-card');
-
-  const a = document.createElement('a');
-  a.href = largeImageURL;
-  a.setAttribute('data-lightbox', 'gallery');
-
-  const img = document.createElement('img');
-  img.src = webformatURL;
-  img.alt = tags;
-  img.loading = 'lazy';
-
-  const info = document.createElement('div');
-  info.classList.add('info');
-
-  function createInfoItem(label, value) {
-    const element = document.createElement('p');
-    element.classList.add('info-item');
-    element.textContent = `${label}: ${value}`;
-    return element;
+    photoCard.appendChild(info);
+  
+    return photoCard;
   }
-
-  info.appendChild(createInfoItem('Likes', likes));
-  info.appendChild(createInfoItem('Views', views));
-  info.appendChild(createInfoItem('Comments', comments));
-  info.appendChild(createInfoItem('Downloads', downloads));
-
-  a.appendChild(img);
-  photoCard.appendChild(a);
-  photoCard.appendChild(info);
-
-  return photoCard;
-}
+  
 
 function displayImages(images) {
   images.forEach((image) => {
